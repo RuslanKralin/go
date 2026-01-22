e7452db554a02a6d43b96087b1f49da75c1be6cc
Ruslan Kralin <ruslan.kralin@intexsoft.by>
2026-01-22 09:01:05 +0300

    save

--- main.go
+++ main.go
@@ -2,17 +2,29 @@ package main
 
 import "fmt"
 
+type User1 struct {
+	Name  string
+	Email string
+	Age   int
+}
+
 
 func main() {
-var num int = 10
+  number :=10
+  var pointer = &number
+  fmt.Println(pointer) // 0xc0000120c8
+  var pointer2 *int
 
-pointer := &num
-fmt.Println(pointer)
-fmt.Println(*pointer)
+  fmt.Println(pointer2) // nil
+ // fmt.Println("разыменование указателя",*pointer2) // будет ошибка, поэтому сначала проводят проверку
+
+  if pointer2 != nil {
+    fmt.Println("указатель обычный", *pointer2)
+  } else {
+    fmt.Println("указатель nil")
+  }
+
 }
 
 
 
-func qwe(a *int) {
-    *a = 20
-}
